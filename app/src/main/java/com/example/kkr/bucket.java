b1d16ff96e0d5bbfa93531c062cae226e2e12901
gayyaalpha <linkinonline48@gmail.com>
2022-01-27 12:50:43 +0530

    String array

--- app/src/main/java/com/example/kkr/bucket.java
+++ app/src/main/java/com/example/kkr/bucket.java
@@ -14,12 +14,18 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+
+// Java program to sort an array
+// using bucket sort
+import java.util.*;
+import java.util.Collections;
 
 public class bucket extends AppCompatActivity {
     ArrayList<Editable> arrayList = new ArrayList<android.text.Editable>();
     ArrayAdapter<Editable> adapter;
     EditText number;
-    Button add;
+    Button add , sort;
     Integer new_array[];
 
     @Override
@@ -33,6 +39,7 @@ public class bucket extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.list);
         number = findViewById(R.id.txt_number);
         add = findViewById(R.id.btn_add);
+        sort=findViewById(R.id.btn_save);
 
         adapter = new ArrayAdapter<Editable>(this, R.layout.list_item,arrayList);
         listView.setAdapter(adapter);
@@ -44,10 +51,62 @@ public class bucket extends AppCompatActivity {
                 adapter.notifyDataSetChanged();
                 number.setText("");
 
-              // new_array = (Integer[]) arrayList.toArray();
+
 
                Toast.makeText(getApplicationContext(), arrayList.toString(), Toast.LENGTH_LONG).show();
             }
         });
+
+        sort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //new_array = (Integer[]) arrayList.toArray();
+                Object array[] = arrayList.toArray();
+                String sArray = Arrays.toString(array);
+                Toast.makeText(getApplicationContext(), sArray, Toast.LENGTH_SHORT).show();
+//                bucketSort();
+//                for(Object o : array) {
+//                    int s = (int) o;
+//
+//
+//                    System.out.println(s);
+//
+            }
+
+        });
+
+    }
+    static void bucketSort(float arr[], int n)
+    {
+        if (n <= 0)
+            return;
+
+        // 1) Create n empty buckets
+        @SuppressWarnings("unchecked")
+        Vector<Float>[] buckets = new Vector[n];
+
+        for (int i = 0; i < n; i++) {
+            buckets[i] = new Vector<Float>();
+        }
+
+        // 2) Put array elements in different buckets
+        for (int i = 0; i < n; i++) {
+            float idx = arr[i] * n;
+            buckets[(int)idx].add(arr[i]);
+        }
+
+        // 3) Sort individual buckets
+        for (int i = 0; i < n; i++) {
+            Collections.sort(buckets[i]);
+        }
+
+        // 4) Concatenate all buckets into arr[]
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < buckets[i].size(); j++) {
+                arr[index++] = buckets[i].get(j);
+            }
+        }
     }
 }
